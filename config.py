845ba7cb0009cf37363e6736084cc7094250cca6
jakob.aio <103487648+7x7ist14@users.noreply.github.com>
2023-03-12 18:32:29 +0100

    Create config.py

---
+++ config.py
@@ -0,0 +1,23 @@
+
+#Input your Discord-Bot token here:
+TOKEN = ''
+
+#Input the Name of the discord channel here (channel name not server name!):
+CHANNEL_NAME = 'scraper'
+
+
+#If you want to change the commands or prefixes you can do it here:
+#(only one command per scraper!)
+COMMAND_PREFIX_ALL = "$scrape"
+
+COMMAND_PREFIX_RESTOCKS = "$restocks"
+
+COMMAND_PREFIX_HYPEBOOST = "$hypeboost"
+
+COMMAND_PREFIX_SNEAKIT = "$sneakit"
+
+COMMAND_PREFIX_PAYOUT_ALL = "$payout"
+
+COMMAND_PREFIX_RESTOCKS_PAYOUT = "$r"
+
+COMMAND_PREFIX_HYPEBOOST_PAYOUT = "$h"
