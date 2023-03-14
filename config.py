170a9413faba40e72e82c9c5932e188ab8269c5c
jakob.aio <103487648+7x7ist14@users.noreply.github.com>
2023-03-14 18:54:00 +0100

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 
 #Input your Discord-Bot token here:
-TOKEN = ''
+TOKEN = 'MTA3ODQwMjA4OTY3MzI5NzkyMA.GSHTAI.VKKlE3Cio0a_8qPSVQl-4T6zsM3MyWhqQ798FI'
 
 #Input the Name of the discord channel here (channel name not server name!):
 CHANNEL_NAME = 'scraper'
@@ -8,16 +8,31 @@ CHANNEL_NAME = 'scraper'
 
 #If you want to change the commands or prefixes you can do it here:
 #(only one command per scraper!)
+
+#All sites together listing prices:
 COMMAND_PREFIX_ALL = "$scrape"
 
+#Listing prices Restocks only:
 COMMAND_PREFIX_RESTOCKS = "$restocks"
 
+#Listing prices Hypeboost only:
 COMMAND_PREFIX_HYPEBOOST = "$hypeboost"
 
+#Listing prices Sneakit only:
 COMMAND_PREFIX_SNEAKIT = "$sneakit"
 
+#All sites together payout prices:
 COMMAND_PREFIX_PAYOUT_ALL = "$payout"
 
+#Payout prices Restocks only:
 COMMAND_PREFIX_RESTOCKS_PAYOUT = "$r"
 
+#Payout prices Hypeboost only:
 COMMAND_PREFIX_HYPEBOOST_PAYOUT = "$h"
+
+#Nike sneaker size chart:
+SIZE_CHART_PREFIX ="$sizes"
+
+#PayPal Fees Calculator:
+PAY_PAL_PREFIX ="$paypal"
+#format -->  $paypal 150€
